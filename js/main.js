10715a3027ee0922183667eb2cfc1464e5623593
Time6628 <pturchan1@gmail.com>
2017-08-28 20:40:29 -0400

    remove everything to be redone later

--- js/main.js
+++
@@ -1,8 +0,0 @@
-var main = function() {
-    $('.box1, .box2, .box3, .box4, .box5, .box6').hover(function() {
-        $(this).fadeTo("slow", 1);
-    }, function() {
-        $(this).fadeTo("slow", .5);
-    });
-};
-$(document).ready(main);
